e190e3eb31d92bf8a1ef718fde6cacd5f866214e
blendh <b.hamiti.9@gmail.com>
2021-12-24 18:56:35 +0100

    add example api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:1337/api/products')
+            .then(res => res.json())
+            .then(res => setData(res));
+    }, []);
+
     return (
         <div>
-            Home
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
         </div>
     );
 }
