085d962deac7ef85fa2602a7448ab80f82a06ef4
Blend Hamiti <b.hamiti.9@gmail.com>
2022-01-08 13:29:29 +0100

    .

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,5 +1,31 @@
 export type User = {
     id: number;
-    name: string,
+    name: string;
+};
 
-}
+export type Product = {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    category: Category;
+    color: {
+        id: number;
+        name: string;
+        hex: string;
+    } | null;
+    sex: {
+        id: number;
+        type: string;
+    } | null;
+    size: {
+        id: number;
+        name: string;
+    } | null;
+};
+
+export type Category = {
+    id: number;
+    Name: string;
+    Description: string;
+};
