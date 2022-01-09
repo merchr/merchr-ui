54104ba580b56048a091c459c66b6f9d87fee571
Blend Hamiti <b.hamiti.9@gmail.com>
2022-01-09 13:59:06 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Account from "./pages/Account";
 import Users from "./pages/Users";
@@ -8,11 +8,53 @@ import { User } from "./util/types";
 import { UserContext } from "./util/userContext";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { USER_EMAIL, USER_PASSWORD } from "./util/constants";
 
 function App() {
     const [user, setUser] = useState<User | null>(null);
 
     const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+    useEffect(() => {
+        if (!user) {
+            const email = localStorage.getItem(USER_EMAIL);
+            const password = localStorage.getItem(USER_PASSWORD);
+
+            if (email && password) {
+                fetch("http://localhost:1337/api/auth/local", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        identifier: email,
+                        password: btoa(password),
+                    }),
+                })
+                    .then((response) =>
+                        response.json().then((response) => {
+                            if (response.error) {
+                                return;
+                            }
+
+                            const { id, username, email, address, phone } =
+                                response.user;
+
+                            setUser({
+                                id,
+                                username,
+                                email,
+                                address,
+                                phone,
+                            });
+                        })
+                    )
+                    .catch((error) => {
+                        console.log("An error occurred:", error.response);
+                    });
+            }
+        }
+    }, []);
 
     return (
         <UserContext.Provider value={value}>
