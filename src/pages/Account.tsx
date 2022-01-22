9346358ac87ca58308e902b591758251db181b20
Blend Hamiti <b.hamiti.9@gmail.com>
2022-01-22 18:23:07 +0100

    .

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -10,8 +10,58 @@ function Account() {
     }
 
     return (
-        <div>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
+        <div className="container">
+            <div className="row my-5">
+                <div className="col fs-1 mx-auto text-center">Account</div>
+            </div>
+            <div className="mx-3" style={{ width: "250px" }}>
+                <h4 className="mb-4" style={{ fontWeight: "normal" }}>
+                    User details
+                </h4>
+                <dl
+                    style={{
+                        display: "flex",
+                        gap: "14px",
+                        flexWrap: "wrap",
+                        flexDirection: "column",
+                    }}
+                >
+                    <div className="def">
+                        <dt>Username:</dt>
+                        <dd>{user.username}</dd>
+                    </div>
+                    <div className="def">
+                        <dt>Name:</dt>
+                        <dd>{user.name}</dd>
+                    </div>
+                    <div className="def">
+                        <dt>Email:</dt>
+                        <dd>{user.email}</dd>
+                    </div>
+                    <div className="def">
+                        <dt>Address:</dt>
+                        <dd>{user.address}</dd>
+                    </div>
+                    <div className="def">
+                        <dt>Phone:</dt>
+                        <dd>{user.phone}</dd>
+                    </div>
+                </dl>
+
+                {/* {user.id && (
+                    <button
+                        
+                        onClick={() => {
+                            localStorage.removeItem(USER_EMAIL);
+                            localStorage.removeItem(USER_PASSWORD);
+                            setUser({ cart: [] });
+                        }}
+                    >
+                        <LogoutOutlinedIcon className="mx-2" />
+                        <ListItemText primary={"Log out"} />
+                    </button>
+                )} */}
+            </div>
         </div>
     );
 }
