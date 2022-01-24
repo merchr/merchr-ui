061bca31a5e0d7f0eefa22b592391c25f3c0386c
elirak <ek27163@seeu.edu.mk>
2022-01-24 10:51:33 +0100

    .

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -8,11 +8,9 @@ function Footer () {
     <div className="footer-dark">
     <footer>
         <div className="container">
-            <div className="row">
-           
-               
-                
-                <div className="col item social"><a href="#"><i className="icon ion-social-facebook"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-instagram"></i></a></div>
+            <div className="row">     
+            {/* Social Medias Icons*/}
+              <div className="col item social"><a href="#"><i className="icon ion-social-facebook"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-instagram"></i></a></div>
             </div>
             <p className="copyright"> Merch © 2022</p>
         </div>
